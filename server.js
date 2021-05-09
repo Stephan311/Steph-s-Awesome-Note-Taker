1b10380119325b8d40fea1ba0523b4ccb8b0f9aa
stephan du Plooy <sdpphotography1@gmail.com>
2021-05-09 14:18:56 -0400

    Ceated route files and added routes

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 
 //installed package
 const express = require('express');
+
+//create the express server
+const app = express();
+
+//the intial port
+ const PORT = process.env.PORT || 8080;
+
+ //Setup express app to handle data parsing
+ app.use(express.urlencoded({ extended: true }));
+ app.use(express.json());
+
+ 
+ 
